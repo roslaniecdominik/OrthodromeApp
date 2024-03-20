ddcdddbe96067ac07e5468bb0ec91a8f6fe10a26
Dominik Roslaniec <roslaniecdominik@gmail.com>
2024-03-20 22:07:02 +0100

    pre load

---
+++ static/loader.js
@@ -0,0 +1,8 @@
+var loader = document.getElementById("preLoader");
+
+window.addEventListener("load", function() {
+    
+    setTimeout(function() {
+        loader.style.display = "none";
+    }, 1000);
+})
